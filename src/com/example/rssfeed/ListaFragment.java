ab3f898b8491ba27ceff810a317e07641f267800
ogranada <ogranada@letmiapp.com>
2013-08-20 18:53:40 -0500

    Tests added

--- src/com/example/rssfeed/ListaFragment.java
+++ src/com/example/rssfeed/ListaFragment.java
@@ -1,22 +1,42 @@
 package com.example.rssfeed;
 
+import com.framework.leopardus.fragments.BaseFragment;
+import com.framework.leopardus.interfaces.InjectMethod;
+import com.framework.leopardus.interfaces.InjectView;
+import com.framework.leopardus.utils.InjectableMethods;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ListaFragment extends Activity {
+public class ListaFragment extends BaseFragment {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_lista_fragment);
+	@InjectView(id=R.id.btntest)
+	Button b;
+	
+	@InjectMethod(id=R.id.btntest)
+	public void onBtn1Click(View v){
+		this.getActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				Toast.makeText(getActivity(), "Click", Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.lista, menu);
-		return true;
+	@InjectMethod(id=R.id.btntest, method=InjectableMethods.OnLongClickListener)
+	public void clickLargoBtn1(View v){
+		this.getActivity().runOnUiThread(new Runnable() {
+			public void run() {
+				Toast.makeText(getActivity(), "Long Click", Toast.LENGTH_LONG).show();
+			}
+		});
 	}
-
+	
+	public ListaFragment() {
+		super(R.layout.activity_lista_fragment);
+	}
+	
 }
--- src/com/example/rssfeed/MainActivity.java
+++ src/com/example/rssfeed/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.rssfeed;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.framework.leopardus.activities.BaseFragmentsActivity;
 import com.framework.leopardus.fragments.BaseFragment;
 import com.framework.leopardus.interfaces.ActivityMethodInterface;
+import com.framework.leopardus.interfaces.InjectMethod;
+import com.framework.leopardus.interfaces.InjectView;
 import com.framework.leopardus.interfaces.InterfacesHelper;
 import com.framework.leopardus.interfaces.MethodInterface;
+import com.framework.leopardus.utils.InjectableMethods;
 import com.framework.leopardus.utils.UI;
 
 public class MainActivity extends BaseFragmentsActivity {
@@ -16,14 +20,7 @@ public class MainActivity extends BaseFragmentsActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		BaseFragment bf = new BaseFragment(R.layout.activity_lista_fragment, new MethodInterface() {
-			
-			@Override
-			public void Method(View v, Object... args) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+		BaseFragment bf = new ListaFragment();
 		
 		setActualFragment(bf);
 		setEnabledMenuOnHome();
@@ -40,6 +37,5 @@ public class MainActivity extends BaseFragmentsActivity {
 				InterfacesHelper.getVoidActivityMethod());
 			}
 		});
-		
 	}
 }
